f940cec0f5e453cb49f02af634b20e2a0d4508e2
杨人瑞 <155858750+YANGRENRUIYRR@users.noreply.github.com>
2024-12-03 20:59:06 +0800

    Update rating.ts

--- api/rating.ts
+++ api/rating.ts
@@ -38,7 +38,7 @@ async function fetchData(username: string): Promise<UserRatingInfo> {
     const user = data.data.user;
     if (user==null) return { rating: 0, text: 'N/A' };
     let rat=0;
-    if(user.problem!=undefined) rat+=user.problem.toFixed;
+    if(user.problem!=undefined) rat+=user.problem;
     if(user.contest!=undefined) rat+=user.contest;
     if(user.contribution!=undefined) rat+=user.contribution;
     if(user.submissions!=undefined) rat+=user.submissions;
